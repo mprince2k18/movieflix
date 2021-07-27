f982aa157a7a9279afeaaeff80fdb7713c5f2024
Mohammad Prince <mprince2k16@gmail.com>
2021-07-27 15:10:21 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,217 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled/models/movie.dart';
+
+void main() {
+  runApp(MovieApp());
+}
+
+// colors
+final primaryColor = Colors.red;
+
+class MovieApp extends StatelessWidget {
+  const MovieApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20.0),
+          ),
+        ),
+        title: Text(
+          'Movieflix',
+          style: TextStyle(
+            letterSpacing: 2.0,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: primaryColor,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              height: 180, // main height
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                children: movies
+                    .map(
+                      (movie) => Container(
+                        margin: EdgeInsets.all(10.0),
+                        width: 300,
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          image: DecorationImage(
+                            image: AssetImage(movie.image),
+                            repeat: ImageRepeat.noRepeat,
+                            fit: BoxFit.cover,
+                            colorFilter: ColorFilter.mode(Colors.grey, BlendMode.darken),
+                          ),
+                        ),
+
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Flexible(
+                                  child: Text(
+                                    movie.name,
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20.0),
+                                  ),
+                                ),
+                                Text(
+                                  movie.category,
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16.0),
+                                ),
+                                Text(
+                                  movie.releaseYear.toString(),
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16.0),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  alignment: Alignment.bottomRight,
+                                  onPressed: () {
+                                    print('prince');
+                                  },
+                                  icon: Icon(
+                                    Icons.whatshot,
+                                    color: Colors.orange,
+                                    size: 24.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+            Column(
+              children: [
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                      child: Text(
+                        'POPULAR LIST',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ))
+              ],
+            ),
+            Container(
+              height: 500, // main height
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                children: movies
+                    .map(
+                      (movie) => Container(
+                        margin: EdgeInsets.all(10.0),
+                        width: 250,
+                        height: 250,
+                        padding: EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          image: DecorationImage(
+                            image: AssetImage(movie.image),
+                            repeat: ImageRepeat.noRepeat,
+                            fit: BoxFit.cover,
+                            colorFilter: ColorFilter.mode(Colors.grey, BlendMode.darken),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Flexible(
+                                  child: Text(
+                                    movie.name,
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20.0),
+                                  ),
+                                ),
+                                Text(
+                                  movie.category,
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16.0),
+                                ),
+                                Text(
+                                  movie.releaseYear.toString(),
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 16.0),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  alignment: Alignment.bottomRight,
+                                  onPressed: () {
+                                    print('prince');
+                                  },
+                                  icon: Icon(
+                                    Icons.whatshot,
+                                    color: Colors.orange,
+                                    size: 34.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/movie.dart
@@ -0,0 +1,28 @@
+final  movies = [
+  Movie(id: 1, name: 'Avengers', category: 'Action/Sci-Fi', releaseYear: 2012, rating: 8.2, image: 'images/avengers.jpg',),
+  Movie(id: 2, name: 'Captain America', category: 'Action/Sci-Fi', releaseYear: 2011, rating: 8.3, image: 'images/captainamerica.jpg'),
+  Movie(id: 3, name: 'Captain Marvel', category: 'Action/Sci-Fi', releaseYear: 2018, rating: 8.7, image: 'images/captainmarvel.jpg'),
+  Movie(id: 4, name: 'Dr Strange', category: 'Action/Sci-Fi', releaseYear: 2017, rating: 7.5, image: 'images/drstrange.jpg'),
+  Movie(id: 5, name: 'Iron Man', category: 'Action/Sci-Fi', releaseYear: 2008, rating: 8.4, image: 'images/ironman.jpg'),
+  Movie(id: 6, name: 'Spider Man', category: 'Action/Sci-Fi', releaseYear: 2016, rating: 8.1, image: 'images/spiderman.jpeg'),
+  Movie(id: 7, name: 'Thor', category: 'Action/Sci-Fi', releaseYear: 2009, rating: 8.0, image: 'images/thor.jpg'),
+];
+
+class Movie {
+  int id;
+  String name;
+  String category;
+  int releaseYear;
+  double rating;
+  String image;
+  bool favourite;
+
+  Movie(
+      {required this.id,
+      required this.name,
+      required this.category,
+      required this.releaseYear,
+      required this.rating,
+      required this.image,
+      this.favourite = false});
+}
